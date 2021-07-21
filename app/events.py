74d95a20d90759d762bdb8e071f4a9cfbf5d5317
Aliaksei Kurakou <aleksey_kurakou@bitnet.by>
2021-07-21 21:36:12 +0300

    start redis onstartup app

--- app/events.py
+++ app/events.py
@@ -4,27 +4,22 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from app.core.config import settings
 from app.db import redis
 from app.db.postgres import postgres
-from app.models.db.room import Base
 
 
 async def create_pg_connection():
     postgres.async_pg_engine = create_async_engine(settings.pg_dsn, echo=True)
-    # async with postgres.async_pg_engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
-    #     await conn.run_sync(Base.metadata.create_all)
 
 
-# async def create_redis_connection():
-#     redis.redis_client = await aioredis.create_redis_pool(settings.REDIS_DSN)
+async def create_redis_connection():
+    redis.redis_client = await aioredis.create_redis_pool(settings.REDIS_DSN)
 
 
 async def close_pg_connection():
     await postgres.async_pg_engine.dispose()
 
 
-#
-# async def close_redis_connection():
-#     await redis.redis_client.close()
+async def close_redis_connection():
+    await redis.redis_client.close()
 
 
 on_startup = [create_pg_connection]
